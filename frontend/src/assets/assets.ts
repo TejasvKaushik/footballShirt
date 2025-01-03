ccf993cf53b6a3f28c3925d0d672572a9cfc8689
Tejasv Kaushik <tejasv2003@gmail.com>
2025-01-03 22:49:59 +0530

    add navbar

---
+++ frontend/src/assets/assets.ts
@@ -0,0 +1,26 @@
+// add images here
+import logo from './logo.png'
+import searchIcon from './search.svg'
+import profileIcon from './profile-circle.svg'
+import cartIcon from './simple-cart.svg'
+import menuIcon from './menu.svg'
+import navArrowDown from './nav-arrow-down.svg'
+
+
+// product attributes
+export const assets = {
+    logo,
+    searchIcon,
+    profileIcon,
+    cartIcon,
+    menuIcon,
+    navArrowDown
+}
+
+
+// products list
+export const products = [
+    {
+        id: "aaaa"
+    }
+]
